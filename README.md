4affd5b0db082a77c8d4abe435d004761b012bb0
Adria Murias <adria.murias@gmail.com>
2022-05-23 02:48:29 +0200

    Update

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 Some population metrics figures for Southern African countries (Angola, Botswana, Eswatini, Lesotho, Malawi, Mozambique, Namibia, South Africa, Zambia & Zimbabwe). Produced from data published at the World Bank Open Data website (https://data.worldbank.org/).
 
+![alt text for screen readers](/figures/plot_sa_pop.png "Population growth")
+
 ![alt text for screen readers](/figures/plot_sa_pop_change.png "Population growth per people living in 1960")
 
 ![alt text for screen readers](/figures/plot_sa_births.png "Fertility rate evolution (births per woman)")
+
+## Plots for Mozambique
+
+![alt text for screen readers](/figures/plot_moz_pop.png "Population growth")
+
+![alt text for screen readers](/figures/plot_moz_births_w.png "Fertility rate evolution (births per woman)")
+
+![alt text for screen readers](/figures/plot_moz_births.png "Births evolution")
--- population_evol.R
+++ population_evol.R
@@ -13,6 +13,12 @@ world_pop <- read_csv('data/API_SP.POP.TOTL_DS2_en_csv_v2_4019998.csv',
                       col_select = c(
                         -`Country Code`,-`Indicator Name`,-`Indicator Code`))
 
+# World Population per Country
+world_w_pop <- read_csv('data/API_SP.POP.TOTL.FE.IN_DS2_en_csv_v2_4030181.csv',
+                      skip = 3,
+                      col_select = c(
+                        -`Country Code`,-`Indicator Name`,-`Indicator Code`))
+
 # Birth Rates per Country
 world_births <- read_csv('data/API_SP.DYN.TFRT.IN_DS2_EN_csv_v2_4023233.csv',
                          skip = 3,
@@ -23,55 +29,134 @@ world_births <- read_csv('data/API_SP.DYN.TFRT.IN_DS2_EN_csv_v2_4023233.csv',
 # Tidy Data
 #------------------------------------------------------------------------------
 
-# Population
-sa_pop <- world_pop[world_pop$`Country Name` == 'Mozambique' |
-                      world_pop$`Country Name` == 'South Africa' |
-                      world_pop$`Country Name` == 'Botswana' |
-                      world_pop$`Country Name` == 'Namibia' |
-                      world_pop$`Country Name` == 'Angola' |
-                      world_pop$`Country Name` == 'Zimbabwe' |
-                      world_pop$`Country Name` == 'Eswatini' |
-                      world_pop$`Country Name` == 'Zambia' |
-                      world_pop$`Country Name` == 'Malawi' |
-                      world_pop$`Country Name` == 'Lesotho',] %>% 
-  select(-`2021`,-`...67`) %>% 
-  pivot_longer(cols = -c(`Country Name`),
-               names_to = 'year',
-               values_to = 'population') %>% 
-  mutate(year = as.double(year))
+# Function to Modify Tibble
+modify_pop_df <- function(data.frame, threshold, opti){
+  
+  a <- data.frame %>%
+    select(-`2021`,-`...67`) %>% 
+    pivot_longer(cols = -c(`Country Name`),
+                 names_to = 'year',
+                 values_to = 'population') %>% 
+    mutate(year = as.double(year))
+  
+  b <-
+    if(missing(threshold)) {
+      a %>% 
+        left_join(
+           y=data.frame %>% 
+             select(-`2021`,-`...67`) %>% 
+             pivot_longer(cols = -c(`Country Name`),
+                          names_to = 'year',
+                          values_to = 'population') %>% 
+             mutate(year = as.double(year)) %>% 
+             group_by(`Country Name`) %>% 
+             summarise(pop_min = min(population)),
+           by='Country Name'
+         ) %>% 
+         mutate(pop_change = population/pop_min,
+                pop_growth = population-pop_min) %>% return()
+    } else {
+      a %>% 
+        filter(year >= threshold) %>%
+                 left_join(
+                   y=data.frame %>% 
+                     select(-`2021`,-`...67`) %>% 
+                     pivot_longer(cols = -c(`Country Name`),
+                                  names_to = 'year',
+                                  values_to = 'population') %>% 
+                     mutate(year = as.double(year)) %>% 
+                     filter(year >= threshold) %>% 
+                     group_by(`Country Name`) %>% 
+                     summarise(pop_min = min(population)),
+                   by='Country Name'
+                 ) %>% 
+                 mutate(pop_change = population/pop_min,
+                        pop_growth = population-pop_min) %>% return()}
+  
+  if(missing(opti)) {
+    return(b)
+  } else {
+    if(deparse(substitute(opti)) == "sa") {
+      b %>% 
+        filter(`Country Name` == 'Mozambique' |
+                 `Country Name` == 'South Africa' |
+                 `Country Name` == 'Botswana' |
+                 `Country Name` == 'Namibia' |
+                 `Country Name` == 'Angola' |
+                 `Country Name` == 'Zimbabwe' |
+                 `Country Name` == 'Eswatini' |
+                 `Country Name` == 'Zambia' |
+                 `Country Name` == 'Malawi' |
+                 `Country Name` == 'Lesotho') %>% 
+        return()
+    } else {"Error: incorrect argument"}
+  }
+}
 
-sa_pop_mod <- sa_pop %>% 
-  left_join(
-    y=sa_pop %>% 
-      group_by(`Country Name`) %>% 
-      summarise(pop_min = min(population)),
-    by='Country Name'
-  ) %>% 
-  mutate(pop_change = population/pop_min,
-         pop_growth = population-pop_min)
+# Tibbles of World and Southern African Countries
+world_pop_mod <- modify_pop_df(world_pop) %>% 
+  left_join(modify_pop_df(world_w_pop) %>% 
+              mutate(w_pop = population) %>% 
+              select(`Country Name`, year, w_pop),
+            by = c("Country Name", "year")) %>% 
+  mutate(w_prop = w_pop/population)
+sa_pop_mod <- modify_pop_df(world_pop, 1, sa)
+sa_w_pop_mod <- modify_pop_df(world_w_pop, 1, sa)
+sa_pop_mod_95 <- modify_pop_df(world_pop, 1995, sa)
+sa_w_pop_mod_95 <- modify_pop_df(world_w_pop, 1995, sa)
+
+moz_pop_mod_95 <- sa_pop_mod_95 %>% 
+  filter(`Country Name` == 'Mozambique') %>% 
+  select(year, population)
+write.csv(moz_pop_mod_95, "data/moz_pop_mod_95.csv", row.names=FALSE)
+
+moz_w_pop_mod_95 <- world_pop_mod %>% 
+  filter(`Country Name` == 'Mozambique',
+         year >= 1995) %>% 
+  select(year, population, w_pop)
+write.csv(moz_w_pop_mod_95, "data/moz_w_pop_mod_95.csv", row.names=FALSE)
 
 # row.names(sa_pop) <- sa_pop %>% pull(`Country Name`)
 # 
 # sa_pop_transp <- t(sa_pop)[-1,] %>% data.frame()
 
 # Birth Rate
-sa_births <- world_births[world_births$`Country Name` == 'Mozambique' |
-                            world_births$`Country Name` == 'South Africa' |
-                            world_births$`Country Name` == 'Botswana' |
-                            world_births$`Country Name` == 'Namibia' |
-                            world_births$`Country Name` == 'Angola' |
-                            world_births$`Country Name` == 'Zimbabwe' |
-                            world_births$`Country Name` == 'Eswatini' |
-                            world_births$`Country Name` == 'Zambia' |
-                            world_births$`Country Name` == 'Malawi' |
-                            world_births$`Country Name` == 'Lesotho',] %>% 
-  select(-`2021`,-`...67`)
-
-sa_births_mod <- sa_births %>% 
+world_births_mod <- world_births %>% 
+  select(-`2021`,-`...67`) %>% 
   pivot_longer(cols = -c(`Country Name`),
                names_to = 'year',
                values_to = 'births per woman') %>% 
   mutate(year = as.double(year))
+
+sa_births_mod <-
+  world_births_mod[world_births_mod$`Country Name` == 'Mozambique' |
+                     world_births_mod$`Country Name` == 'South Africa' |
+                     world_births_mod$`Country Name` == 'Botswana' |
+                     world_births_mod$`Country Name` == 'Namibia' |
+                     world_births_mod$`Country Name` == 'Angola' |
+                     world_births_mod$`Country Name` == 'Zimbabwe' |
+                     world_births_mod$`Country Name` == 'Eswatini' |
+                     world_births_mod$`Country Name` == 'Zambia' |
+                     world_births_mod$`Country Name` == 'Malawi' |
+                     world_births_mod$`Country Name` == 'Lesotho',]
+
+moz_births_mod_95 <- world_births_mod %>% 
+  filter(`Country Name` == 'Mozambique',
+         year >= 1995) %>% 
+  select(year, `births per woman`)
+write.csv(moz_births_mod_95, "data/moz_births_mod_95.csv", row.names=FALSE)
+
+# Moz
+moz_pop_births_w <- world_pop_mod %>% 
+  filter(`Country Name` == 'Mozambique') %>% 
+  select(year, population, w_pop) %>% 
+  left_join(world_births_mod %>% 
+              filter(`Country Name` == 'Mozambique') %>% 
+              select(year, `births per woman`),
+            by = 'year') %>% 
+  mutate(births = round(w_pop * `births per woman`))
+
+write.csv(moz_pop_births_w, "data/moz_pop_births_w.csv", row.names=FALSE)
 
 #------------------------------------------------------------------------------
 # Plot
@@ -82,25 +167,40 @@ plot_sa_pop <- sa_pop_mod %>%
   ggplot(aes(x=year, y=population, color = `Country Name`)) +
   geom_line()
 
+ggsave("figures/plot_sa_pop.png",
+       plot_sa_pop, width = 8, height = 4)
+
 plot_sa_pop_change <- sa_pop_mod %>% 
   ggplot(aes(x=year, y=pop_change, color = `Country Name`)) +
-  geom_line()
+  geom_line() +
+  ylab("Population/Initial population")
+
+ggsave("figures/plot_sa_pop_change.png",
+       plot_sa_pop_change, width = 8, height = 4)
 
 plot_sa_pop_growth <- sa_pop_mod %>% 
   ggplot(aes(x=year, y=pop_growth, color = `Country Name`)) +
   geom_line()
+
+ggsave("figures/plot_sa_pop_growth.png",
+       plot_sa_pop_growth, width = 8, height = 4)
 
 # Fertility rate (births per woman) evolution of 10 SSA countries
 plot_sa_births <- sa_births_mod %>% 
   ggplot(aes(x=year, y=`births per woman`, color = `Country Name`)) +
   geom_line()
 
+ggsave("figures/plot_sa_births.png",
+       plot_sa_births, width = 8, height = 4)
+
 ## SINCE 1995
 # Population evolution of 10 SSA countries
-plot_sa_pop_change_95 <- sa_pop_mod %>% 
-  filter(year >= 1995) %>% 
+plot_sa_pop_change_95 <- sa_pop_mod_95 %>% 
   ggplot(aes(x=year, y=pop_change, color = `Country Name`)) +
   geom_line()
+
+ggsave("figures/plot_sa_pop_change_95.png",
+       plot_sa_pop_change_95, width = 8, height = 4)
 
 # Fertility rate (births per woman) evolution of 10 SSA countries
 plot_sa_births_95 <- sa_births_mod %>% 
@@ -108,13 +208,27 @@ plot_sa_births_95 <- sa_births_mod %>%
   ggplot(aes(x=year, y=`births per woman`, color = `Country Name`)) +
   geom_line()
 
-#------------------------------------------------------------------------------
-# Adjust for Moz
-#------------------------------------------------------------------------------
+ggsave("figures/plot_sa_births_95.png",
+       plot_sa_births_95, width = 8, height = 4)
 
-# Population evolution of 10 SSA countries
-plot_moz_pop_95 <- sa_pop_mod %>% 
-  filter(year >= 1995 & `Country Name` == 'Mozambique') %>% 
-  ggplot(aes(x=year, y=population, color = `Country Name`)) +
-  geom_line() + 
-  geom_smooth(method='lm', formula= y~x)
+# Mozambique
+plot_moz_pop <- moz_pop_births_w %>% 
+  ggplot(aes(x=year, y=population)) +
+  geom_line()
+
+ggsave("figures/plot_moz_pop.png",
+       plot_moz_pop, width = 8, height = 4)
+
+plot_moz_births_w <- moz_pop_births_w %>% 
+  ggplot(aes(x=year, y=`births per woman`)) +
+  geom_line()
+
+ggsave("figures/plot_moz_births_w.png",
+       plot_moz_births_w, width = 8, height = 4)
+
+plot_moz_births <- moz_pop_births_w %>% 
+  ggplot(aes(x=year, y=births)) +
+  geom_line()
+
+ggsave("figures/moz_pop_births.png",
+       plot_moz_births, width = 8, height = 4)
